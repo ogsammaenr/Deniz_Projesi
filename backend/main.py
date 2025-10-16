7a179537cc47c54ad46f019459c62bdd3e65a7bf
ogsammaenr <osmangamer152@gmail.com>
2025-10-16 15:11:01 +0300

    fix print sea data class

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 from data.seaData import _fetchSeaData
 from data.printSeaData import printSeaData
 
-_fetchSeaData()
+#_fetchSeaData()
 
 FILE_PATH = "src/data/copernicus_data/merged_bgc_dataset.nc"
 
